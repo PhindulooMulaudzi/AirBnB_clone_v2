c35cc463ecb636ccdc0d676eed8a78507a5c7315
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2024-01-13 12:33:36 +0200

    Implement Requirement 6. DBStorage - States and Cities
    Add dbStorage and update State, City and BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+import models
+import uuid
+
+if models.storage_type == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    if models.storage_type == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models import storage_type
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if storage_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if storage_type != "db":
+        @property
+        def cities(self):
+            """list of city instances related to the state"""
+            try:
+                all_cities = models.storage.all(models.City)
+                city_list = [city for city in all_cities.values()
+                             if city.state_id == self.id]
+                return city_list
+            except Exception as e:
+                print(f"Error getting cities for state: {e}")
+                return None
