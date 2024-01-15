1c23b120a23bfd724c1997d99ebc55b4a0f66c10
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2024-01-15 09:45:13 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,7 +32,7 @@ class FileStorage:
         """Delete obj from __objects if it exists."""
         try:
             if obj is not None:
-                key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                key = f"{obj.__class__.__name__}.{obj.id}"
                 if key in self.__objects:
                     del self.__objects[key]
                     self.save()
